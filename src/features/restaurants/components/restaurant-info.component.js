ee108c3f99d32f1052935642b5bb37c4477256d6
Michael Saylor <michael_saylor@carmax.com>
2023-03-10 08:17:14 -0500

    Layout restaurant info card

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,14 +1,30 @@
-import { Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
 
-export const RestaurantInfo = ({ resturant }) => {
+export const RestaurantInfo = ({ resturant = {} }) => {
   const {
     name = "Some Restaurant",
     icon,
-    photos,
-    address,
-    openingHours,
-    rating,
-    ifClosedTemporarily,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    ifClosedTemporarily = false,
   } = resturant;
-  return <Text>info</Text>;
+
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Card.Content>
+        <Text>{name}</Text>
+      </Card.Content>
+    </Card>
+  );
 };
+
+const styles = StyleSheet.create({
+  card: { backgroundColor: "white" },
+  cover: { padding: 16, backgroundColor: "white" },
+});

dbb8682ae014919ed3aac36dc77b43fe88b25802
Michael Saylor <michael_saylor@carmax.com>
2023-03-10 15:35:32 -0500

    setup theme

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 

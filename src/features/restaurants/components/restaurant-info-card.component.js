0572351152ecf476d99ffd74abcfd48e3369bda3
Michael Saylor <michael_saylor@carmax.com>
2023-03-10 08:20:30 -0500

    clean up code after rename

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,7 +1,8 @@
+import React from "react";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 
-export const RestaurantInfo = ({ resturant = {} }) => {
+export const RestaurantInfoCard = ({ resturant = {} }) => {
   const {
     name = "Some Restaurant",
     icon,
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import React from "react";
+import { StyleSheet, View, SafeAreaView, StatusBar } from "react-native";
 import { Searchbar } from "react-native-paper";
-import { RestaurantInfo } from "../components/restaurant-info.component";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
 export const RestaurantsScreen = () => (
   <SafeAreaView style={styles.safeAreaView}>
@@ -8,7 +9,7 @@ export const RestaurantsScreen = () => (
       <Searchbar placeholder="Search" />
     </View>
     <View style={styles.list}>
-      <RestaurantInfo />
+      <RestaurantInfoCard />
     </View>
   </SafeAreaView>
 );

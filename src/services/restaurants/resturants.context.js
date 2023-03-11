3073feb468c891e1eaddaaf613bbe1cbb3913124
Michael Saylor <michael_saylor@carmax.com>
2023-03-11 18:38:00 -0500

    setup restaurants context

---
+++ src/services/restaurants/resturants.context.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useMemo, createContext } from "react";
+import { restaurantRequest, restaurantsTransform } from "./restaurants.service";
+
+export const RestaurantsContext = createContext();
+
+export const RestaurantsContextProvider = ({ children, value }) => {
+  return (
+    <RestaurantsContext.Provider value={{ restaurants: [1, 2, 3] }}>
+      {children}
+    </RestaurantsContext.Provider>
+  );
+};

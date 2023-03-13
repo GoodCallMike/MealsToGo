24476d4e51a0515a7b8de552743997e45c0c6ca6
Michael Saylor <michael_saylor@carmax.com>
2023-03-13 12:58:55 -0400

    store favorites in async storage

--- src/services/favorites/favorites.context.js
+++ src/services/favorites/favorites.context.js
@@ -1,9 +1,31 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavoritesContext = createContext();
 
 export const FavoritesContextProvider = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
+
+  const saveFavorites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@favorites", jsonValue);
+    } catch (e) {
+      console.log("error storing", e);
+    }
+  };
+
+  const loadFavorites = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@favorites");
+
+      if (value) {
+        setFavorites(JSON.parse(value));
+      }
+    } catch (e) {
+      console.log("error loading", e);
+    }
+  };
 
   const add = (restaurant) => setFavorites([...favorites, restaurant]);
 
@@ -13,6 +35,14 @@ export const FavoritesContextProvider = ({ children }) => {
     );
     setFavorites(newFavorites);
   };
+
+  useEffect(() => {
+    loadFavorites();
+  }, []);
+
+  useEffect(() => {
+    saveFavorites(favorites);
+  }, [favorites]);
 
   return (
     <FavoritesContext.Provider
